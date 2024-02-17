9001e8c30cff1be068209c7efe6be6eecd760c54
leatile77 <moilaleatile@gmail.com>
2024-02-17 17:04:51 +0200

    fixed the logic of removing first node and how memory is freed

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -83,7 +83,7 @@ if __name__ == '__main__':
     user_2.password = 89
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
-
+i
     if not user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
 password")
